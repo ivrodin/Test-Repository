79692d30051182fcb4603a703c8dbe37bd1a258e
Ivan Rodin <rodin.ivan97@gmail.com>
2023-10-19 18:04:21 +0300

    1

--- currentTask.py
+++ currentTask.py
@@ -6,11 +6,19 @@ can only take one positional parameter.
 
 For example:
 
->>> @decorator_apply(lambda user_id: user_id + 1)
->>> def return_user_id(num: int): 
-        return num
->>> return_user_id(42) 
->>> 43
+@decorator_apply(lambda user_id: user_id + 1)
+def return_user_id(num: int): 
+    return num
+return_user_id(42) 
+43
 """
 
+def decorator_apply(func):
+    def func1(user_id):
+        return user_id
+
+
+@decorator_apply(lambda user_id: user_id + 1)
+def return_user_id(num: int):    
+    return num
 
