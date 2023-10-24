13f1a519d976b37c6156e5e434e7b379ac0ee492
Ivan Rodin <rodin.ivan97@gmail.com>
2023-10-24 19:40:08 +0400

    2

--- myAttempt.py
+++ myAttempt.py
@@ -1,5 +1,5 @@
 from abc import ABC
-# from abc import abstractmethod
+from abc import abstractmethod
 
 
 class Vehicle(ABC):
@@ -16,7 +16,7 @@ class Vehicle(ABC):
         self.mileage = mileage
         
         
-
+    @abstractmethod
     def wheels_num(self) -> int:
         return 0
 
@@ -25,7 +25,7 @@ class Vehicle(ABC):
 
 
     def is_motorcycle(self) -> bool:
-        if (self.__class__).__name__ == 'Motorcycle':
+        if self.wheels_num() == 2:
             return True
         else:
             return False
@@ -63,16 +63,16 @@ class Bus(Vehicle):
         return 6
 
 
-vehicles = (
-    Car(brand_name="Toyota", year_of_issue=2020, base_price=1_000_000, mileage=150_000),
-    Motorcycle(brand_name="Suzuki", year_of_issue=2015, base_price=800_000, mileage=35_000),
-    Truck(brand_name="Scania", year_of_issue=2018, base_price=15_000_000, mileage=850_000),
-    Bus(brand_name="MAN", year_of_issue=2000, base_price=10_000_000, mileage=950_000)
-)
+# vehicles = (
+#     Car(brand_name="Toyota", year_of_issue=2020, base_price=1_000_000, mileage=150_000),
+#     Motorcycle(brand_name="Suzuki", year_of_issue=2015, base_price=800_000, mileage=35_000),
+#     Truck(brand_name="Scania", year_of_issue=2018, base_price=15_000_000, mileage=850_000),
+#     Bus(brand_name="MAN", year_of_issue=2000, base_price=10_000_000, mileage=950_000)
+# )
 
-for vehicle in vehicles:
-        print(
-            f"Vehicle type={vehicle.vehicle_type()}\n"
-            f"Is motorcycle={vehicle.is_motorcycle()}\n"
-            f"Purchase price={vehicle.purchase_price}\n"
-        )
+# for vehicle in vehicles:
+#         print(
+#             f"Vehicle type={vehicle.vehicle_type()}\n"
+#             f"Is motorcycle={vehicle.is_motorcycle()}\n"
+#             f"Purchase price={vehicle.purchase_price}\n"
+#         )
