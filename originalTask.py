029361c0da2c2f83e920fbc37fa64e76e1e6c828
Ivan Rodin <rodin.ivan97@gmail.com>
2023-10-27 14:17:31 +0400

    2

--- originalTask.py
+++ originalTask.py
@@ -1,45 +1,15 @@
 class Pagination:
     def __init__(self, data, items_on_page):
-        # self.data = data
-        # self.items_on_page = items_on_page
-        self.data_list = []
-        temp_str = ''
-        count = 1
-        for item in data:
-            if count == items_on_page or item == data[len(data)-1]:
-                temp_str += item
-                self.data_list.append(temp_str)
-                temp_str = ''
-                count = 1
-            else:
-                temp_str += item
-                count += 1
+        pass
 
     def page_count(self):
-        for res, item in enumerate(self.data_list):
-            pass
-        return res + 1
+        pass
 
     def count_items_on_page(self, page_number):
-        for res, item in enumerate(self.data_list[page_number - 1]):
-            pass
-        return res + 1
+        pass
 
     def find_page(self, data):
-        found_pages = []
-        for char_data in data:
-            for pg_count, item in enumerate(self.data_list):
-                for char_item in item:
-                    if char_data == char_item:
-                        pass
-                
-
-
+        pass
     
     def display_page(self, page_number):
         pass
-
-a = Pagination('I wanna be your dog', 3)
-print(a.data_list)
-print(a.page_count())
-print(a.count_items_on_page(2))
