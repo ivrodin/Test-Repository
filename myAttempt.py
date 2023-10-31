0f9a06078f3f6311cd44fa9315f36316e22e7c3d
Ivan Rodin <rodin.ivan97@gmail.com>
2023-10-31 18:52:34 +0300

    2

--- myAttempt.py
+++ myAttempt.py
@@ -4,7 +4,7 @@ class Pagination:
         temp_str = ''
         count = 1
         for item in data:
-            if count == items_on_page or item == data[len(data)-1]:
+            if count == items_on_page:
                 temp_str += item
                 self.data_list.append(temp_str)
                 temp_str = ''
@@ -12,47 +12,66 @@ class Pagination:
             else:
                 temp_str += item
                 count += 1
+        if temp_str:
+            self.data_list.append(temp_str)
 
     def page_count(self):
-        return len(self.data_list)
+        print(len(self.data_list) - 1)
+        return len(self.data_list) - 1
 
     def count_items_on_page(self, page_number):
-        return len(self.data_list[page_number - 1])
+        try:
+            print(len(self.data_list[page_number - 1]))
+            return len(self.data_list[page_number - 1])
+        except:
+            print('Exception: Invalid index. Page is missing.')
+
 
     def find_page(self, data):
         result_pages = set()
         temp_str = ''
         temp_pages = set()
         i = 1
-        for pg_count, item in enumerate(self.data_list):
-            for char_item in item:
-                temp_str += char_item
-                if temp_str[0:i] == data[0:i]:
-                    if temp_str != data:
-                        i += 1
-                        if char_item == item[len(item)-1]:
-                            temp_pages.add(pg_count + 1)
-                        continue
+        try:
+            for pg_count, item in enumerate(self.data_list):
+                for index, char_item in enumerate(item):
+                    temp_str += char_item
+                    if temp_str[0:i] == data[0:i]:
+                        if temp_str != data:
+                            i += 1
+                            if index + 1 == len(item):
+                                temp_pages.add(pg_count)
+                            continue
+                        else:
+                            if temp_pages:
+                                for page in temp_pages:
+                                    result_pages.add(page)
+                                temp_pages.clear()
+                            result_pages.add(pg_count)
+                            temp_str = ''
                     else:
-                        if temp_pages != 0:
-                            for page in temp_pages:
-                                result_pages.add(page)
-                            temp_pages.clear()
-                        result_pages.add(pg_count + 1)
-                        temp_str = ''
-                else:
-                    temp_str = char_item
-                    i = 2
-        return list(result_pages)
-            
-
-                    
-                
-
+                        temp_str = char_item
+                        if char_item == data[0]:
+                            temp_pages.add(pg_count)
+                            continue
+                        i = 2
+                        temp_pages.clear()
+            if result_pages:
+                print(list(result_pages))
+                return list(result_pages)
+            else:
+                raise Exception
+        except:
+            print(f'Exception: {data} is missing on the pages')
 
     
     def display_page(self, page_number):
-        pass
+        try:
+            print(self.data_list[page_number])
+            return self.data_list[page_number]
+        except:
+            print('Exception: Invalid index. Page is missing.')
+
 
 # a = Pagination('I wanna be your dog', 3)
 # print(a.data_list)
@@ -60,8 +79,9 @@ class Pagination:
 # print(a.count_items_on_page(7))
 # print(a.find_page('wanna'))
 
-a = Pagination('wanna anna nana panar', 3)
+a = Pagination('wanna anna nana pana', 3)
 print(a.data_list)
-print(a.page_count())
-print(a.count_items_on_page(7))
-print(a.find_page('an'))
+a.page_count()
+a.count_items_on_page(10)
+a.find_page('nnapa')
+a.display_page(10)
