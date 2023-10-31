0d0cc8bf548ae3957ac6f7ea0c22a0710358ca87
Ivan Rodin <rodin.ivan97@gmail.com>
2023-11-01 02:12:34 +0300

    1

--- myAttempt.py
+++ myAttempt.py
@@ -4,4 +4,12 @@ from typing import List
 class Counter:
     def __init__(self, values: List[int]):
         self.values = values
-    # TODO: add your code here
+    
+    def __add__(self, value):
+        res_values = []
+        for item in self.values:
+            res_values.append(f"{str(item)} {value}")
+        return res_values
+
+s = Counter([1, 2, 3]) + "mississippi"
+print(s)
