76aeffa4e31eacf2e0fb01f1c51b9ffa5660c9e1
Ivan Rodin <rodin.ivan97@gmail.com>
2023-11-04 00:57:14 +0400

    1

--- myAttempt.py
+++ myAttempt.py
@@ -1,14 +1,42 @@
+from typing import Any
+
+
 class Bird:
-    pass
+    def __init__(self, name) -> None:
+        self.name = name
+    
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        print(f'{self.name} can fly and walk')
+
+    def fly(self):
+        print(f'{self.name} can fly')
+
+    def walk(self):
+        print(f'{self.name} can walk')
 
 
-class FlyingBird:
-    pass
+class FlyingBird(Bird):
+    def __init__(self, name, ration = 'grains') -> None:
+        self.name = name
+        self.ration = ration
+
+    def eat(self):
+        print(f"{self.name} eats mostly {self.ration}")
 
 
 class NonFlyingBird:
-    pass
+    def __init__(self, name, ration = 'grains') -> None:
+        self.name = name
+        self.ration = ration
+
+    def eat(self):
+        print(f"{self.name} eats mostly {self.ration}")
 
 
 class SuperBird:
-    pass
+    def __init__(self, name, ration = 'grains') -> None:
+        self.name = name
+        self.ration = ration
+
+    def eat(self):
+        print(f"{self.name} eats mostly {self.ration}")
