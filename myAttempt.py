e77452ee6177f1f1e794c3114789d3e348f9fc6e
Ivan Rodin <rodin.ivan97@gmail.com>
2023-11-04 17:10:25 +0300

    2

--- myAttempt.py
+++ myAttempt.py
@@ -2,26 +2,75 @@ from typing import Any
 
 
 class Bird:
-    pass
-
-
-class FlyingBird:
-    pass
-
-
-class NonFlyingBird:
-    def __init__(self, name, ration = 'grains') -> None:
+    def __init__(self, name) -> None:
         self.name = name
+    
+    def __str__(self) -> str:
+        return f'{self.name} can walk and fly'
+
+    def fly(self):
+        return f'{self.name} can fly'
+
+    def walk(self):
+        return f'{self.name} can walk'
+
+
+
+
+class FlyingBird(Bird):
+    def __init__(self, name, ration = 'grains') -> None:
+        super().__init__(name)
         self.ration = ration
 
     def eat(self):
-        print(f"{self.name} eats mostly {self.ration}")
+        return f"{self.name} eats mostly {self.ration}"
 
 
-class SuperBird:
+
+
+class NonFlyingBird(Bird):
+
     def __init__(self, name, ration = 'grains') -> None:
-        self.name = name
+        super().__init__(name)
         self.ration = ration
 
+    def __str__(self) -> str:
+        return f'{self.name} can walk, swim and fly'
+    
+    def swim(self):
+        return f'{self.name} can swim'
+    
+    def fly(self):
+        raise AttributeError (f"{self.name} object has no attribute 'fly'")
+
     def eat(self):
-        print(f"{self.name} eats mostly {self.ration}")
+        return f"{self.name} eats mostly {self.ration}"
+
+
+
+
+class SuperBird(FlyingBird, NonFlyingBird):
+    
+    def __init__(self, name, ration='grains') -> None:
+        super().__init__(name, ration)
+    
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return super().__call__(*args, **kwds)
+    
+
+b = Bird('Any')
+print(b.walk())
+print(str(b))
+
+p = NonFlyingBird('Penguin', 'fish')
+print(p.swim())
+print(p.fly())
+print(p.eat())
+
+c = FlyingBird('Cannary')
+print(str(c))
+print(c.eat())
+
+s = SuperBird('Gull')
+print(str(s))
+print(s.eat())
