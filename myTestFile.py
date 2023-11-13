8eaf00fb02671f62a079f62bb3fcda7b906d1402
Ivan Rodin <rodin.ivan97@gmail.com>
2023-11-13 14:37:02 +0300

    1

--- myTestFile.py
+++ myTestFile.py
@@ -1,37 +1,9 @@
-class NotChangableDescriptor:
-
-    @classmethod
-    def validate_instance_name(cls, instance, name):
-        if not getattr(instance, name, None):
-            raise TypeError
-
-    def __set_name__(self, owner, name):
-        self.name = '_' + name
-
-    def __get__(self, instance, owner = None):
-        return getattr(instance, self.name)
-    
-    def __set__(self, instance, value):
-        print(instance.__dict__)
-        # self.validate_instance_name(instance, self.name)
-        if instance.__dict__.get(self.name, None) is None:
-            setattr(instance, self.name, value)
-
-class TestClass:
-    a = NotChangableDescriptor()
-    b = NotChangableDescriptor()
-
-    def __init__(self, a, b) -> None:
-        self.a = a
-        self.b = b
 
 
-tst = TestClass(1,2)
-print(tst.__dict__)
-tst.a = 3 
+s = 'abcd'
 
-print(tst.__dict__)
+a =s[1].upper() + s[2:]
 
-# print(tst.__a)
+a.upper()
 
-# print(TestClass.a) # 'NoneType' object has no attribute '__a'
+print(a)
