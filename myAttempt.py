92b9059a978613fa902208ae01e9c92d63fdfb27
Ivan Rodin <rodin.ivan97@gmail.com>
2023-11-14 15:23:44 +0300

    1

--- myAttempt.py
+++ myAttempt.py
@@ -1,23 +1,25 @@
 import os
-from pathlib import Path
 
 class Cd:
     
     def __init__(self, init_path:str) -> None:
-        self.init_path = os.path.normpath(init_path)
+        self.init_path = init_path[1:]
         self.location = os.getcwd()
+        self.path = os.path.join(self.location, self.init_path)
 
     def __enter__(self):
-        try:
-            os.chdir(self.init_path)
-        except:
+        print(self.path)
+        print (os.path.isdir(self.path))
+        if os.path.isdir(self.path):
+            os.chdir(self.path)
+        else:
             raise ValueError ('No such directory')
         
     def __exit__(self, exc_type, exc_value, exc_traceback):
         os.chdir(self.location)
 
 
-with Cd("C:\Users\ivanr\OneDrive\Рабочий стол\Other's"):
+with Cd('/testDir'):
     with open('testDoc.txt', 'r') as f:
         print(f.read())
 
