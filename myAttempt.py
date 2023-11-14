ff093aab514d0688daf9f3233966bab8098d4175
Ivan Rodin <rodin.ivan97@gmail.com>
2023-11-14 14:17:35 +0300

    1

--- myAttempt.py
+++ myAttempt.py
@@ -2,5 +2,25 @@ import os
 
 
 class Cd:
-    pass
-    # TODO: please add your code here
+    
+    def __init__(self, init_path) -> None:
+        self.init_path = init_path
+        self.location = os.getcwd()
+
+    def __enter__(self):
+        try:
+            os.chdir(self.init_path)
+        except:
+            raise ValueError ('No such directory')
+        
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        os.chdir(self.location)
+
+
+# with Cd(r"C:\Users\ivanr\OneDrive\Рабочий стол\Other's"):
+#     with open('testDoc.txt', 'r') as f:
+#         print(f.read())
+
+with Cd(r"C:\Users\ivanr\OneDriv\Рабочий стол\Other's"):
+    with open('testDoc.txt', 'r') as f:
+        print(f.read())
