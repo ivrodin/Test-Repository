570aff51e366b53192ffa6d66fdf0f093d62d7c6
Ivan Rodin <rodin.ivan97@gmail.com>
2023-11-14 12:38:34 +0300

    1

--- myAttempt.py
+++ myAttempt.py
@@ -1,6 +1,19 @@
 import os
 import shutil
+import random
 
 class TempDir:
-    pass
-    # TODO: please ad your code here
+
+    location = os.getcwd()
+    temp_directory = 'temporary_directory_' + str(random.randint(0, 9))
+    path = os.path.join(location, temp_directory)
+
+    def __enter__(self):
+        os.mkdir(self.path)
+        os.chdir(self.path)
+        return self.path
+
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        os.chdir(self.location)
+        shutil.rmtree(self.path)
