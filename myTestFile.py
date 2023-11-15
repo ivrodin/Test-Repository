a3d204a0f2fd43369654082901e9aa7297980883
Ivan Rodin <rodin.ivan97@gmail.com>
2023-11-16 01:13:53 +0400

    1

--- myTestFile.py
+++ myTestFile.py
@@ -1,32 +1,87 @@
-from contextlib import ContextDecorator
-from datetime import datetime, timedelta
-from timeit import default_timer as timer
+from collections import OrderedDict
+
+class Cipher:
+
+    natural_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    def __init__(self, key) -> None:
+        self.key = ''.join(OrderedDict.fromkeys(key))
+        self.natural_alphabet_dict = self.alphabet_dictionarization(self.natural_alphabet)
+        self.encripted_alphabet = self.alphabet_encription()
+        self.encripted_alphabet_dict = self.alphabet_dictionarization(self.encripted_alphabet)
 
 
-class LogFile(ContextDecorator):
+        # print(self.key)
+        print(self.natural_alphabet_dict)
+        print(self.encripted_alphabet_dict)
+        # print(self.encripted_alphabet)
+        # print(self.natural_alphabet)
+        # print(self.encripted_alphabet)
+        # print(len(self.encripted_alphabet) == len(self.natural_alphabet))
 
-    def __init__(self, name) -> None:
-        super().__init__()
-        self.log_file_name = name
+    def alphabet_encription(self): 
+        _alphabet_copy = self.natural_alphabet
+        for key_elem in self.key:
+            for alph_elem in _alphabet_copy:
+                if key_elem == alph_elem:
+                    _alphabet_copy = _alphabet_copy.replace(alph_elem, '')
+        return self.key + _alphabet_copy
 
-    def __enter__(self):
-        self.start_time = datetime.now().isoformat()
-        self.timer_start = timer()
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.exc_val = exc_val
-        self.run_time = timedelta(seconds = timer() - self.timer_start)
-        with open (self.log_file_name, 'a') as f:
-            f.write(f'Start: {self.start_time} | Run: {self.run_time} | An error occurred: {self.exc_val}')
-            f.write('\n')
-        return False
+    def alphabet_dictionarization(self, alphabet):
+        alphabet_dict = {}
+        for key, val in enumerate(alphabet):
+            alphabet_dict[key] = val
+        return alphabet_dict
     
+    def encode(self, data):
+        pass
 
-@LogFile('my_trace.log')
-def sum_of_ints(a, b):
-    return a / b
 
-sum_of_ints(2,1)
+a = Cipher('proto')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# natural_alph = 'abcdefghijklmnopqrstuvwxyz'
+
+# key = 'banana'
+
+# clean_key = key_duplicates_removal(key)
+
+# print(clean_key)
+
+# encr_alph = aplhabet_encription(clean_key, natural_alph)
+
+# print(encr_alph)
+
+# print(len(natural_alph) == len(encr_alph))
+
+
+
+
+# natural_alph = 'protabcdefghijklmnqsuvwxyz'
+
+# nat_alph_dict = {}
+# for val, key in enumerate(natural_alph):
+#     nat_alph_dict[key] = val
+
+# print(nat_alph_dict)
+
+
+
+
+
+
 
 
