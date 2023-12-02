2a917144792184181f002a37908e6e3cb0855b75
Ivan Rodin <rodin.ivan97@gmail.com>
2023-12-02 17:23:58 +0400

    2

--- myTestFile.py
+++ myTestFile.py
@@ -14,9 +14,40 @@ def parse_cli():
     res_parse_dict = args.__dict__
     res_parse_dict['text_content'] = response.text
 
+    
+    with open ('my_rss_feed.txt', 'w') as f:
+        f.write(response.text)
+
     return res_parse_dict
 
-a = parse_cli()
-print(a)
+# a = parse_cli()
+
+def item_formater(data):
+    temp_str = ''
+    formated_txt = ''
+    appender_flag = False
+    for elem in data:
+        if appender_flag is True or  elem == '<':
+            temp_str += elem
+            appender_flag = True
+            if elem == '>':
+                appender_flag = False
+                if temp_str == '<item>' or temp_str == '</item>':
+                    formated_txt += '\n' + temp_str + '\n' + '\t'
+                    temp_str = ''
+            continue
+        formated_txt += elem
+    return formated_txt
+
+my_text = '<item>hello</item><item>world<item></item><item>im alive</item>'
+
+my_new_text = item_formater(my_text)
+
+print(my_new_text)
+
+
+
+
+
 
 
